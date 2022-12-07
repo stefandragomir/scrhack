5aaf05450081b86c22edcc64b4d715936b466676
stefan.dragomir <stefan.dragomir.000@gmail.com>
2022-12-07 21:13:53 +0200

    added dota marker detection in image processing

--- src/shck.py
+++ src/shck.py
@@ -1,11 +1,20 @@
 
 import sys
 import os
+import io
+import base64
+import time
 import urllib.request
+import imagehash
+
+from PIL import Image
 from bs4 import BeautifulSoup
 
 
-GPLAY_SCR_URL = "http://scr.gplay.ro/"
+GPLAY_SCR_URL  = "http://scr.gplay.ro/"
+WEBSERVER_WAIT = 30 #seconds
+
+IMAGE_MARKER_VERSION = b'/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAUAHgDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD5/ooooAKtWdhd38pjs7ae4kClikMZcgDqSB2qrW34d12bw34gsdWtWzJayq+OfmX+JfxGQaAM63sbu7WVre2mmEMZllMaFtiDqzY6D3NdNH4VtdN0iHUfEFzNC9zF59tp9tHvmaLoJJGPESHsSCT2FdJ4ssLHwdp+pz6XKkkfiR1+wiNsFLD5ZXHHTLlUwe0bVv67L4tuvHseu+D5ruTQ9SNvOktsx+zKqxqrLOB8q7dpBDYwO2c0AeUeGdAm8TeI7XRoHSJ7hjmR+kaqpZifoAT+FdJZ+GfDXiVbyz8P3+qNqVpbvNCt7GgS8VBllTaco2OQDn3qxrr3XhnxwfGPhy2hGlT6hMmnuu1o5tvyyAIDnacsB046VqC9sPCkl14hk0aHRNTktpYLLSxO8kpkkG1pHU/6qNBuwpAJzQBgw+GPDmi6ZpVz4pv9QgutViFxBDYxI32eEnCySFjk5wTtXnHfNZWv+DdU0jxXJoVvBJqNw3zwfZYjIZ4yu4MoXJPHJx0rrvEugan47sPC+reHbV78DTIdPuY4sH7NLHkEMP4VOcgnjg1tanptj4i1LVZrCa+1Sbw/p9ppy2ukzCOS8xw77trEoD2APTsOSAeZ6F4VvtW8XWnh64RrC7nlCMLlChj4ycg85wOh61txeGNA1RNbttNl1e01DSbWW6b7eibJBFwwO3BjbPAySM8V2VzY2mr+MPAWoakkllp8umiFJPtTL/pELSbYmlJ3Bs7BnIPpjtPaah4m1w6xp/xA0cWmitC8k2oPAbcwFATEFk6SjOAFyxPByecgHg1FFFABRRRQAUUUUAFFFFAFmd3dYldiwVMKD/CPvYHoMkn8aRJZNuxXZUkb5lU4BoooAcQVWJgzAk569D7VH/y1Cdmxk96KKAJEkkRAEkZRIMMFOARnH8qRJZElGx2VsH5lOD+YoooAcnyxDuM459+v8hRcTSkeU0rsiE7QWJxRRQBUooooAKKKKAP/2Q=='
 
 """*************************************************************************************************
 ****************************************************************************************************
@@ -139,20 +148,69 @@ class SCHK_Html():
 """*************************************************************************************************
 ****************************************************************************************************
 *************************************************************************************************"""
-class SCHK_Image():
+class SCHK_Screenshot(SCHK_Html):
 
     def __init__(self):
 
-        self.name = ""
-        self.url  = ""
-        self.path = ""
-        self.data = None
+        self.name    = ""
+        self.url     = ""
+        self.path    = ""
+        self.image   = None
+        self.is_dota = False
 
     def save(self):
 
-        with open(self.path,'wb+') as _image:
+        self.data.save(self.path)
 
-            _image.write(self.data)
+    def load_from_file(self):
+
+        self.image = Image.open(self.path)
+
+        self.image.load()
+
+    def load_from_html(self):
+
+        _bytes = self.read_html_bytes(self.url)
+
+        self.image = Image.open(io.BytesIO(_bytes))
+
+    def to_base64(self,image):
+
+        _buffer = io.BytesIO()
+
+        image.save(_buffer, format="JPEG")
+
+        return base64.b64encode(_buffer.getvalue())
+
+    def from_base64(self,data):
+
+        _bytes = base64.b64decode(data)
+
+        image = Image.open(io.BytesIO(_bytes))
+
+        return image
+
+    def compare(self,image1,image2):
+
+        _hash_1     = imagehash.average_hash(image1)
+        _hash_2     = imagehash.average_hash(image2)
+        _hash_limit = 5
+
+        return (_hash_1 - _hash_2) < _hash_limit
+
+    def is_dota_marker(self):
+
+        _mk_vs_rect = (1760,10,1880,30)
+
+        _img_mk_vs_1 = self.image.crop(_mk_vs_rect)
+
+        _img_mk_vs_2 = self.from_base64(IMAGE_MARKER_VERSION)
+
+        return self.compare(_img_mk_vs_1, _img_mk_vs_2)
+
+    def process(self):
+
+        self.is_dota = self.is_dota_marker()
 
     def __repr__(self):
 
@@ -249,14 +307,17 @@ class SCHK_User(SCHK_Html):
 
             self.log.write("[{}/{}]downloading screenshot [{}] for user [{}]".format(_count,len(_img_list),_img_name,self.name))
 
-            _img      = SCHK_Image()
+            _img      = SCHK_Screenshot()
             _img.name = _img_name
             _img.url  = "{}/{}".format( self.url, urllib.parse.quote(_img_name) )
             _img.path = os.path.join( self.path_dw, _img_name )
 
-            _img.data = self.read_html_bytes(_img.url)
+            _img.load_from_html()
 
             _img.save()
+
+            #wait to trick the web server
+            time.sleep(WEBSERVER_WAIT)
 
             self.images.add(_img)
 
@@ -297,4 +358,12 @@ class SCHK():
 *************************************************************************************************"""
 if __name__ == '__main__':
 
-    SCHK().run()
+    # SCHK().run()
+
+    _img = SCHK_Screenshot()
+
+    _img.path = r"c:\Users\stefa\.schck\dw\`Nameless`\01-09-2022 22-45-50.jpg"
+
+    _img.load_from_file()
+
+    _img.process()
